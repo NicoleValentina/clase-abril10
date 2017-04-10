d66658386ffe5374ddbcf2cfb5d2d22986e468ec
NicoleValentina <nivacea@gmail.com>
2017-04-10 12:41:02 -0300

    Add files via upload
    arregla3

--- p5/cero.js
+++ p5/cero.js
@@ -6,9 +6,9 @@ function setup() {
 
 function draw() {
   for ( var x=0; x< windowWidth; x+=10) {
-    for  (var y=0; y< windowWidth; x+=10 ) {
+    for  (var y=0; y< windowWidth; y+=10 ) {
       rectMode(CENTER);
-      line(x,y,5,5);    
+      rect(x,y,5,5);    
   }
 }
 }
